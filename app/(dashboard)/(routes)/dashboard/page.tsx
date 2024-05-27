3e8463451dee95462a59f4a06d8c1a46e29dd0d7
Pratik-Kumar-621 <pratikkumar0621@gmail.com>
2024-05-27 23:02:16 +0530

    Added page structure and setup auth using clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <div>Dashboard</div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default DashboardPage;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <div>Landing Page</div>
+      <Link href="/sign-in">
+        <Button>Login</Button>
+      </Link>
+      <Link href="/sign-up">
+        <Button>Register</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default LandingPage;
