382af48e88a52816b54d295eb58f96873fcc8977
Pratik-Kumar-621 <pratikkumar0621@gmail.com>
2024-05-28 22:46:30 +0530

    added private route

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
-
+const isPrivateRoute = createRouteMatcher(["profile"]);
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
